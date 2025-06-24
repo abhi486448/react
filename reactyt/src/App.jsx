547ff60ff25278da5d13eede02fe56df4212a364
abhi486448 <ag096129abhishek@gmail.com>
2025-06-24 20:43:31 +0530

    add api

--- reactyt/src/App.jsx
+++ reactyt/src/App.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 
 const App = () => {
 
-  const getDate =()=>{
-    console.log("Hello")
+  const [data, setData] = useState( [] )
+
+  const getDate = async ()=>{
+    const response = await axios.get('https://picsum.photos/v2/list')
+    setData(response.data)
+    console.log(data)
   }
+
+  useEffect(() => {
+    getDate()
+  
+
+  }, [])
+  
   return (
     <div className='p-10'>
-      <button onClick={getDate} className='active:scale-90 bg-teal-700 text-white font-semibold text-2xl px-6 py-3 rounded'>Get Data</button>
+      {/* <button onClick={getDate} className='active:scale-90 bg-teal-700 text-white font-semibold text-2xl px-6 py-3 rounded'>Get Data</button> */}
       <div className='p-5 mt-5 bg-black'>
-
+          {data.map(function(elem,idx){
+            return <div key={idx} className='bg-gray-50 text-black flex items-center justify-between w-full px-7 py-6 rounded mb-3'>
+              <img className='h-40' src={elem.download_url} alt="" />
+              <h1>{elem.author}</h1>
+            </div>
+          })}
       </div>
     </div>
   )
